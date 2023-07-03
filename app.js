04acbc3ba5f6c1442e71ed2679835c4d81849dc4
Suryadeep36 <75900590+Suryadeep36@users.noreply.github.com>
2023-07-03 15:52:16 +0530

    Updated app.js port

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const _ = require('lodash');
 const capitalize = require('lodash.capitalize');
 const mongoose = require("mongoose");
 const app = express();
-
+let port = process.env.PORT;
+if(port == null || port == ""){
+  port = 3000;
+} 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -120,8 +123,8 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server started on port "+port);
 });
 
 
